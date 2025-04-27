71392d92d2f0a28095d40298b3572931c78fc919
faraslutfi <127936502+faraslutfi@users.noreply.github.com>
2025-04-27 18:23:35 +0700

    menambahkan api mahasiswa

---
+++ modul10tp_1302204106/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace modul10tp_1302204106
+{
+	public class Mahasiswa
+	{
+		public string Nama { get; set; }
+		public string Nim { get; set; }
+
+        public Mahasiswa()
+        {
+            Nama = string.Empty;
+            Nim = string.Empty;
+        }
+    }
+
+}
+
+
---
+++ modul10tp_1302204106/MahasiswaController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modul10tp_1302204106.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Faras Irzan", Nim = "1302204106" },
+            new Mahasiswa { Nama = "Stephen Curry", Nim = "1302000002" }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Mahasiswa>> Get()
+        {
+            return daftarMahasiswa;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> Get(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+            return daftarMahasiswa[index];
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Mahasiswa mahasiswa)
+        {
+            daftarMahasiswa.Add(mahasiswa);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+            daftarMahasiswa.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
+
